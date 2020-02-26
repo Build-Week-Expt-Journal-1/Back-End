28f2d39114b24b7bb91aee3fbd6c628a138d3c5c
RyanSWest <west56309@gmail.com>
2020-02-25 23:29:49 -0600

    started migrations and tables

---
+++ database/migrations/20200225231523_places.js
@@ -0,0 +1,44 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("places", tbl => {
+      tbl.increments();
+      tbl.text("city", 128);
+      tbl.text("country", 128).notNullable();
+      tbl
+        .integer("user_id")
+        .notNullable()
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl.text("photo_url");
+    })
+
+    .createTable("stories", tbl => {
+      tbl.increments();
+      tbl
+        .integer("place_id")
+        .notNullable()
+        .unsigned()
+        .references("id")
+        .inTable("places");
+      onUpdate("CASCADE").onDelete("CASCADE");
+      tbl.text("story_photo_url");
+      tbl.string("review").notNullable();
+      tbl
+        .integer("user_id")
+        .notNullable()
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("stories")
+    .dropTableIfExists("restaurants");
+};
