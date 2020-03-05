f0c06fd2b3f09ee29a53eadc0a399bd5a1b949d4
RyanSWest <west56309@gmail.com>
2020-03-05 00:08:58 -0600

    1 thing

--- api/routes/users/userRouter.js
+++ api/routes/users/userRouter.js
@@ -7,11 +7,13 @@ const router = require("express").Router();
 
 router.get('/all', (req,res)=> {
   Users.getUsers()
-  .then(user=> res.json(200).json(user))
+  .then(user=> {
+    res.status(200).json(user);
 })
 .catch(err=> {
   res.status(500).json({message: 'error getting the users.'})
-})
+});
+});
  
 
 router.post('/register',(req,res)=> {
