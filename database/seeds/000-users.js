00e0727be11cd0658e614c0a2ad82f27e9f12fe9
RyanSWest <west56309@gmail.com>
2020-03-04 23:14:24 -0600

    again seed

--- database/seeds/000-users.js
+++ database/seeds/000-users.js
@@ -1,11 +1,11 @@
-
+const bcrypt = require('bcrypt')
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('users').del()
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'Bobby',password:123} 
+        {id: 1, username: 'Bobby',password:bcrypt.hashSync("123",10)} 
       
       ]);
     });
